17be17cfff7c68f27648cb651949af28bbe2a1f4
Olha Danylevska <odanilevskaya2@gmail.com>
2024-02-01 19:36:35 +0000

    changed pool

--- server/server.js
+++ server/server.js
@@ -12,14 +12,8 @@ dotenv.config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false,
-    require: true,
-  },
+  connectionString: process.env.DB_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 pool.connect((err, client, done) => {
